f30acfa009560da56c00f7a2500303bf02bd4c3f
ldlh615 <1207893654@qq.com>
2022-03-26 23:02:24 +0800

    add

--- assets/main.js
+++ assets/main.js
@@ -1,12 +1,46 @@
 document.addEventListener("DOMContentLoaded", init);
 
+let clientHeight = 0;
+let navEle = undefined;
+let themeEle = undefined;
+
+function getClientHeight() {
+  let height = 0;
+  if (document.body.clientHeight && document.documentElement.clientHeight) {
+    height =
+      document.body.clientHeight < document.documentElement.clientHeight
+        ? document.body.clientHeight
+        : document.documentElement.clientHeight;
+  } else {
+    height =
+      document.body.clientHeight > document.documentElement.clientHeight
+        ? document.body.clientHeight
+        : document.documentElement.clientHeight;
+  }
+  return height;
+}
+
 function init() {
   console.log("document ready!");
+  initData();
   initNav();
+  initScroll();
+}
+
+function initData() {
+  clientHeight = getClientHeight();
+  navEle = document.querySelector(".header-bar");
+  const theme = localStorage.getItem("theme");
+  if (theme) {
+    const html = document.querySelector("html");
+    html.setAttribute("class", theme);
+  }
 }
 
 function initNav() {
   const navitems = document.querySelectorAll(".nav .item");
+  const themeEle = document.querySelector(".header-bar .theme");
+
   if (navitems) {
     for (let item of navitems) {
       item.onclick = function (e) {
@@ -25,4 +59,24 @@ function initNav() {
       };
     }
   }
+
+  if (themeEle) {
+    themeEle.onclick = function () {
+      const html = document.querySelector("html");
+      const htmlThemeClass = html.getAttribute("class");
+      const val = htmlThemeClass === "light-mode" ? "" : "light-mode";
+      html.setAttribute("class", val);
+      localStorage.setItem("theme", val);
+    };
+  }
+}
+
+function initScroll() {
+  window.onscroll = function (e) {
+    if (document.scrollingElement.scrollTop >= clientHeight) {
+      navEle.setAttribute("class", "header-bar scroll");
+    } else {
+      navEle.setAttribute("class", "header-bar");
+    }
+  };
 }
